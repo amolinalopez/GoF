81707233c9e94924af27dd171ed5d46a0b9a44ce
Adriana Molina <amolinalopez05@gmail.com>
2022-07-28 21:36:23 +0200

    end burger for now ?

--- candies.js
+++ candies.js
@@ -1,1 +1,10 @@
 console.log('Le tombeau des Lucioles Team')
+
+const icone = document.querySelector('.navbar-mobile i');
+const modal = document.querySelector('.modal');
+
+icone.addEventListener('click', function(){
+    modal.classList.toggle('change-modal')
+    icone.classList.toggle('fa-times')
+})
+
